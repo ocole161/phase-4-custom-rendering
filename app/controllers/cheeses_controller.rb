8c9ae7cb931c8e82efa80c7dbd44364727ae817a
Owen <ocole161@gmail.com>
2023-02-21 16:24:08 -0700

    commit tree 5a81b768093ebc2c9b297947c2ecf36fc2c9a0ad

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -3,7 +3,7 @@ class CheesesController < ApplicationController
   # GET /cheeses
   def index
     cheeses = Cheese.all
-    render json: cheeses
+    render json: cheeses, only: [:id, :name]
   end
 
   # GET /cheeses/:id
